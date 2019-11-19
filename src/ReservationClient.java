0a3589c4cb9056d868e1a9c089ab5ff9a4fd407c
thatprakhar <pnahar@purdue.edu>
2019-11-18 23:38:03 -0500

    Try JFrame

--- src/ReservationClient.java
+++ src/ReservationClient.java
@@ -8,24 +8,29 @@ public class ReservationClient {
     public static void main(String[] args) throws IOException {
 
         final String TITLE = "Purdue University Flight Reservation System";
+        JFrame parent = new JFrame(TITLE);
+        parent.setSize(500,500);
+        parent.getDefaultCloseOperation();
+        parent.setVisible(true);
 
-        String host = JOptionPane.showInputDialog(null, "What is your hostname you'd like to connect to?", "Hostname?", JOptionPane.PLAIN_MESSAGE);
+
+        String host = JOptionPane.showInputDialog(parent, "What is your hostname you'd like to connect to?", "Hostname?", JOptionPane.PLAIN_MESSAGE);
         int port;
         boolean connected = false;
 
         while (!connected) {
             try {
-                port = Integer.parseInt(JOptionPane.showInputDialog(null, "What is the port you'd like to connect to?", "Port?", JOptionPane.PLAIN_MESSAGE));
+                port = Integer.parseInt(JOptionPane.showInputDialog(parent, "What is the port you'd like to connect to?", "Port?", JOptionPane.PLAIN_MESSAGE));
                 Socket clientSocket = new Socket(host, port);
                 connected = true;
             } catch (ConnectException ce) {
-                JOptionPane.showMessageDialog(null, "Please enter a valid port", "Invalid Port", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(parent, "Please enter a valid port", "Invalid Port", JOptionPane.PLAIN_MESSAGE);
             }
         }
         ImageIcon purdue_icon = new ImageIcon("purdue_icon.png");
         String[] options = {"Exit", "Book a Flight"};
-        JOptionPane.showOptionDialog(null, "<html> <b> Welcome to the Purdue University Airline <br> Reservation Management System! </b> <br>  </html>", TITLE, 0, JOptionPane.PLAIN_MESSAGE, purdue_icon, options, null);
+        JOptionPane.showOptionDialog(parent, "<html> <b> Welcome to the Purdue University Airline <br> Reservation Management System! </b> <br>  </html>", TITLE, 0, JOptionPane.PLAIN_MESSAGE, purdue_icon, options, null);
         String[] options2 = {"Exit", "Yes, I want to book a flight."};
-        JOptionPane.showOptionDialog(null, "<html> <b> Do you want to book flight today? <html>", TITLE, 0, JOptionPane.PLAIN_MESSAGE, null, options2, null);
+        JOptionPane.showOptionDialog(parent, "<html> <b> Do you want to book flight today? <html>", TITLE, 0, JOptionPane.PLAIN_MESSAGE, null, options2, null);
     }
 }
